7eee176d3102163cee5810be8ba375fb33861bb5
Pedro Felippe <pedfelippe@gmail.com>
2021-11-02 22:19:49 -0300

    feat#1 - estruturação inicial do projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import styles from "../styles/home.module.css";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio - Portal FN</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
+  );
 }
